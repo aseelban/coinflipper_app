793291d36950fe05b88ca349833446237e2afbf4
aseelban <aseelban@gmail.com>
2020-03-14 08:32:44 +0300

    Updates

---
+++ precache-manifest.1a7e9969e54673440a60fa27d9ab4314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe45181ba76f31ea98167f0511b18231",
+    "url": "/coinflipper_app/index.html"
+  },
+  {
+    "revision": "3b9cd862b6e4f45b98b2",
+    "url": "/coinflipper_app/static/css/main.7f371d0a.chunk.css"
+  },
+  {
+    "revision": "22e59645e710862ac629",
+    "url": "/coinflipper_app/static/js/2.6ef6f0e1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/coinflipper_app/static/js/2.6ef6f0e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b9cd862b6e4f45b98b2",
+    "url": "/coinflipper_app/static/js/main.0eedcdd5.chunk.js"
+  },
+  {
+    "revision": "58ed25f2d12bfc81d6a4",
+    "url": "/coinflipper_app/static/js/runtime-main.cbf828e9.js"
+  }
+]);
